c9e8ea108c8ad23decb55fbb5fb5c9c72e156244
Suheb Ahmed <suhebahmedabdullah@gmail.com>
2026-04-05 22:37:19 +0600

    Implented CSS for Countries

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,18 +1,21 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
 
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
-    
+
     return (
         <div>
             <h1>In the Countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country country = {country}></Country>)
-            }
+            <div className='countries'>
+                {
+                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
+
+    const visitHandler = () => {
+        console.log('Visited')
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h1>Name: {country.name.common} </h1>
+            <h2>Name: {country.name.common} </h2>
             <p>Populations: {country.population.population}</p>
+            <p>Area: {country.area.area + " --- "} {country.area.area > 300000 ? "Big Country" : 
+                "Small Country"}</p>
+            <button onClick={visitHandler}>Not Visited</button> 
         </div>
     );
 };
 
 export default Country;  
+
