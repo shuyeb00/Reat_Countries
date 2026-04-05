e90ae021870a7cde8e522a18500187c8ca688a8e
Suheb Ahmed <suhebahmedabdullah@gmail.com>
2026-04-05 23:40:14 +0600

    state lift up partially done and used handlers as props

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,19 +1,26 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css'
 
 
 const Countries = ({ countriesPromise }) => {
 
+    const [VisitedCountries, setVisitedCountries] = useState([])
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
+
+    const handleVisitedCountries = (country) => {
+        console.log("Visited Countries Clicked", country)
+    }
 
     return (
         <div>
             <h1>In the Countries: {countries.length}</h1>
+            <h2>Total Visited Countries: {VisitedCountries.length}</h2>
             <div className='countries'>
                 {
-                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                    countries.map(country => <Country handleVisitedCountries={handleVisitedCountries}
+                        key={country.cca3.cca3} country={country}></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
+
+    const [visited, setVisited] = useState(false)
 
     const visitHandler = () => {
-        console.log('Visited')
+        setVisited(!visited)
+        handleVisitedCountries(country)
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited-country'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common} </h2>
             <p>Populations: {country.population.population}</p>
             <p>Area: {country.area.area + " --- "} {country.area.area > 300000 ? "Big Country" : 
                 "Small Country"}</p>
-            <button onClick={visitHandler}>Not Visited</button> 
+            <button onClick={visitHandler}>{visited ? 'Visited' : 'Not Visited'}</button> 
         </div>
     );
 };
 
-export default Country;  
+export default Country;   
 
